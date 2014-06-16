cd96c58beb0326a8faa42f5ba01a85c45f0f0505
dev <dev@n0.dev>
2014-06-16 05:56:49 -0400

    Update

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ hadoop fs -rm -r $OUTPUTDIR
 
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming-*.jar \
   -D mapred.job.reduces=0 \
-  -libjars Formats/Formats.jar \
   -inputformat org.apache.hadoop.mapred.SequenceFileAsBinaryInputFormat \
   -outputformat org.apache.hadoop.mapred.TextOutputFormat \
   -file $SCRIPT \
---
+++ setupHDFS.sh
@@ -0,0 +1,4 @@
+hadoop fs -mkdir -p wellbook/production-raw
+hadoop fs -mkdir -p wellbook/injections-raw
+hadoop fs -mkdir -p wellbook/logs-raw
+hadoop fs -mkdir -p wellbook/scout-tickets-raw
